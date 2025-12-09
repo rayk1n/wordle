b8264b8e9dafcbfa6cfb86a47487cc13f989d738
Raiymbek Bexultanuly <raykin.2005@gmail.com>
2025-12-10 04:19:20 +0500

    Added the skeleton of the functions, implemented load_dictionary

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from wordle!")
+import random
 
+def load_dictionary(file_path):
+    with open(file_path) as f:
+        words = [line.strip() for line in f]
+    return words
+
+def is_valid_guess(guess, guesses):
+    print("")
+
+def evaluate_guess(guess, word):
+    print("")
+
+def wordle(guesses, answers):
+    print("")
+
+def main():
+    answers = load_dictionary("answers.txt")
+    guesses = load_dictionary("guesses.txt")
+
+    print(random.choice(answers))
+    print(random.choice(guesses))
 
 if __name__ == "__main__":
     main()
